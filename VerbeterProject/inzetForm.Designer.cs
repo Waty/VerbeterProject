486f4f8ab16b6ae05db121428ca5423e8cf17454
Waty <wartaarnoutse@gmail.com>
2014-10-14 14:18:26 +0200

    Initial commit

---
+++ VerbeterProject/inzetForm.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace VerbeterProject
+{
+    partial class inzetForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDeelnemer1 = new System.Windows.Forms.Label();
+            this.lblDeelnemer2 = new System.Windows.Forms.Label();
+            this.lblDeelnemer3 = new System.Windows.Forms.Label();
+            this.lblDeelnemer4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblWinnaar = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "John";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "James";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(25, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 24);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(25, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Luke";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(25, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Anna";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(117, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(117, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(117, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(65, 29);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(117, 177);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(65, 29);
+            this.textBox4.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.lblDeelnemer4);
+            this.groupBox1.Controls.Add(this.lblDeelnemer3);
+            this.groupBox1.Controls.Add(this.lblDeelnemer2);
+            this.groupBox1.Controls.Add(this.lblDeelnemer1);
+            this.groupBox1.Location = new System.Drawing.Point(241, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 456);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Deelnemers";
+            // 
+            // lblDeelnemer1
+            // 
+            this.lblDeelnemer1.AutoSize = true;
+            this.lblDeelnemer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeelnemer1.Location = new System.Drawing.Point(6, 53);
+            this.lblDeelnemer1.Name = "lblDeelnemer1";
+            this.lblDeelnemer1.Size = new System.Drawing.Size(171, 24);
+            this.lblDeelnemer1.TabIndex = 0;
+            this.lblDeelnemer1.Text = "Nr 1: MasterChief";
+            // 
+            // lblDeelnemer2
+            // 
+            this.lblDeelnemer2.AutoSize = true;
+            this.lblDeelnemer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeelnemer2.Location = new System.Drawing.Point(6, 175);
+            this.lblDeelnemer2.Name = "lblDeelnemer2";
+            this.lblDeelnemer2.Size = new System.Drawing.Size(170, 24);
+            this.lblDeelnemer2.TabIndex = 1;
+            this.lblDeelnemer2.Text = "Nr 2: Jack Carver";
+            // 
+            // lblDeelnemer3
+            // 
+            this.lblDeelnemer3.AutoSize = true;
+            this.lblDeelnemer3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeelnemer3.Location = new System.Drawing.Point(6, 286);
+            this.lblDeelnemer3.Name = "lblDeelnemer3";
+            this.lblDeelnemer3.Size = new System.Drawing.Size(156, 24);
+            this.lblDeelnemer3.TabIndex = 2;
+            this.lblDeelnemer3.Text = "Nr 3: Big Daddy";
+            // 
+            // lblDeelnemer4
+            // 
+            this.lblDeelnemer4.AutoSize = true;
+            this.lblDeelnemer4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeelnemer4.Location = new System.Drawing.Point(6, 397);
+            this.lblDeelnemer4.Name = "lblDeelnemer4";
+            this.lblDeelnemer4.Size = new System.Drawing.Size(286, 24);
+            this.lblDeelnemer4.TabIndex = 3;
+            this.lblDeelnemer4.Text = "Nr 4: Ezio Auditore di Firenza";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VerbeterProject.Properties.Resources.masterchief;
+            this.pictureBox1.Location = new System.Drawing.Point(303, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 98);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::VerbeterProject.Properties.Resources.carver;
+            this.pictureBox2.Location = new System.Drawing.Point(303, 132);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(96, 98);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::VerbeterProject.Properties.Resources.bigdaddy;
+            this.pictureBox3.Location = new System.Drawing.Point(303, 236);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(96, 98);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::VerbeterProject.Properties.Resources.ezio;
+            this.pictureBox4.Location = new System.Drawing.Point(303, 340);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(96, 98);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 54);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Race Simulation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblWinnaar
+            // 
+            this.lblWinnaar.AutoSize = true;
+            this.lblWinnaar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinnaar.Location = new System.Drawing.Point(98, 350);
+            this.lblWinnaar.Name = "lblWinnaar";
+            this.lblWinnaar.Size = new System.Drawing.Size(25, 24);
+            this.lblWinnaar.TabIndex = 11;
+            this.lblWinnaar.Text = "...";
+            // 
+            // inzetForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 511);
+            this.Controls.Add(this.lblWinnaar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "inzetForm";
+            this.Text = "Inzetten!";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblDeelnemer4;
+        private System.Windows.Forms.Label lblDeelnemer3;
+        private System.Windows.Forms.Label lblDeelnemer2;
+        private System.Windows.Forms.Label lblDeelnemer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblWinnaar;
+    }
+}
+
---
+++ VerbeterProject/inzetForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VerbeterProject
+{
+    public partial class inzetForm : Form
+    {
+        public inzetForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random randomizer = new Random();
+
+            //genereer een random nummer tussen 1 en 4, dit is de winnaar van de race
+            int winnaar = randomizer.Next(1, 4);
+
+            //zet het nummer van de winnaar in het label
+            lblWinnaar.Text = winnaar.ToString();
+
+            //kleur de naam van de winnaar rood
+            if (winnaar == 1)
+            {
+                lblDeelnemer1.ForeColor = Color.Red;
+            }
+            else if (winnaar == 2)
+            {
+                lblDeelnemer2.ForeColor = Color.Yellow;
+            }
+            else if (winnaar == 3)
+            {
+                lblDeelnemer1.ForeColor = Color.Red;
+            }
+            else if (winnaar == 5)
+            {
+                lblDeelnemer4.ForeColor = Color.Red;
+            }
+
+            //kleur de naam van degenen die goed gegokt hebben groen
+            int gokVanJohn = Convert.ToInt32(textBox1.Text);
+            int gokVanJames = Convert.ToInt32(textBox3.Text);
+            int gokVanLuke = Convert.ToInt32(textBox3.Text);
+            int gokVanAnna = Convert.ToInt32(textBox4.Text);
+
+            if (gokVanJohn == winnaar)
+            {
+                label1.ForeColor = Color.Green;
+            }
+            if (gokVanJames == winnaar)
+            {
+                label2.ForeColor = Color.Green;
+            }
+            if (gokVanLuke == winnaar)
+            {
+                label3.ForeColor = Color.Green;
+            }
+            if (gokVanAnna == winnaar)
+            {
+                label4.ForeColor = Color.Green;
+            }
+            
+        }
+    }
+}
